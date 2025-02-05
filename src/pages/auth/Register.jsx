42e5dcb59fd74bea5c69ecf63e5d50c05fe0e172
TamjidBhai <tamtop2000@gmail.com>
2025-02-05 23:17:38 +0600

    added auth

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./auth.css";
 import PageHeader from "../../components/pageHeader/PageHeader";
 import AuthHeader from "../../components/authHeader/AuthHeader";
+import useFormFields from "../../hooks/useFormFields";
+import { useDispatch, useSelector } from "react-redux";
+import { createUser } from "../../features/auth/authApiSlice";
+import { getAuthData, setMessageEmpty } from "../../features/auth/authSlice";
+import { createToast } from "../../utils/toast";
 
 const Register = () => {
+  //hooks
+  const dispatch = useDispatch();
+  const { message, error } = useSelector(getAuthData);
+
+  //form field manage
+  const { input, handleInputChange, resetForm } = useFormFields({
+    name: "",
+    auth: "",
+    password: "",
+  });
+
+  //handle user register
+  const handleUserRegister = (e) => {
+    e.preventDefault();
+
+    dispatch(createUser(input));
+
+    resetForm();
+  };
+
+  useEffect(() => {
+    if (message) {
+      createToast(message, "success");
+      dispatch(setMessageEmpty());
+    }
+
+    if (error) {
+      createToast(error);
+      dispatch(setMessageEmpty());
+    }
+  }, [message, error]);
   return (
     <>
       <PageHeader title="Create Your Account" />
@@ -16,10 +52,28 @@ const Register = () => {
               desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
             />
             <div className="auth-form">
-              <form action="">
-                <input type="text" placeholder="Your Name" />
-                <input type="text" placeholder="Email or Phone number" />
-                <input type="password" placeholder="Password" />
+              <form onSubmit={handleUserRegister}>
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  value={input.name}
+                  name="name"
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Email or Phone number"
+                  value={input.auth}
+                  name="auth"
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={input.password}
+                  name="password"
+                  onChange={handleInputChange}
+                />
                 <button>Create Account</button>
               </form>
             </div>
